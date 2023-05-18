568b4a7729ffe131fd99335d266dbb57259bd271
f-Rodeh <rodeh.dev@gmail.com>
2023-05-17 18:00:10 -0600

    Add generateGrid function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // access the container div
-// create a row div that contains numberOfRows rows
-// append the row div inside the container numberOfColumns times
+const container = document.querySelector('.grid-container');
+
+generateGrid( 16 , 16 );
+
+function generateGrid( numberOfRows, numberOfColumns ) {
+
+  // create a div that represents a pixel
+  const px = document.createElement('div');
+
+  // create a row div that contains numberOfColumns pixels
+  const row = document.createElement('div');
+  for (let i = 0; i < numberOfColumns; i++) {
+    console.log('appending px to row')
+    row.appendChild(px)
+  }
+
+  // append the row div inside the container numberOfRows times
+  for (let i = 0; i < numberOfRows; i++) {
+    console.log('appending row to container');
+    container.appendChild(row);
+  }
+}
